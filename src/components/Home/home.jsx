23f13567c71c91bd44f3ef65d06cdba11c210f26
nicknamejv <jonvercabral@yahoo.com>
2021-09-29 22:43:47 -0700

    adjusting image in home page

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
     return (
         <div className="container-fluid p-5 d-flex justify-content-center flex-wrap" id="home">
             <div className="row g-5">
-                <div className="col-md-4">
-                    <img src={image} className="img-fluid" alt="home" id="home-img"></img>
+                <div className="col-md-4" id="home-img">
+                    <img src={image} className="img-fluid" alt="home"></img>
                 </div>
                 <div className="col-md-8 d-flex justify-content-center align-items-center flex-column text-center">
                     <p className="display-5 home-text" >
